f441bb027585e779c596ce1f646aac2c580954a8
Yahia <yahiazakaria369@gmail.com>
2024-10-31 13:08:11 +0300

    Add project files.

---
+++ Survey.Basket.Api/Program.cs
@@ -0,0 +1,44 @@
+
+using Survey.Basket.Api.Middelwares;
+using Survey.Basket.Api.test;
+
+namespace Survey.Basket.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webApplicationBuilder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            webApplicationBuilder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            webApplicationBuilder.Services.AddEndpointsApiExplorer();
+            webApplicationBuilder.Services.AddSwaggerGen();
+
+            webApplicationBuilder.Services.AddKeyedScoped<IoperationOS, Windos>("Win");
+            webApplicationBuilder.Services.AddKeyedScoped<IoperationOS, IOS>("ios");
+
+            var app = webApplicationBuilder.Build();
+
+           
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+     
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
